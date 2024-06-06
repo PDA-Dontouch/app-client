5d70952319d1b50af234016c3d41a522ef65a204
yjp8842 <yjp20183029@gmail.com>
2024-06-06 14:20:37 +0900

    feat: create button

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import './App.css';
 // import BottomUpModal from './components/common/Modal/BottomUpModal';
 import BasicModal from './components/common/Modal/BasicModal';
+import Button from './components/common/Button';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>모달 오픈</button>
-      {/* {isOpen && <BottomUpModal onClose={() => setIsOpen(false)} />} */}
-      {isOpen && <BasicModal onClose={() => setIsOpen(false)} />}
+      {/* <button onClick={() => setIsOpen(true)}>모달 오픈</button>
+      {isOpen && <BottomUpModal onClose={() => setIsOpen(false)} />}
+      {isOpen && <BasicModal onClose={() => setIsOpen(false)} />} */}
+      <Button name='구매하기' status='active' onClick={() => {}} />
     </>
   );
 }

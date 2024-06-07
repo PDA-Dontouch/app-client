72f36dd821e835faad2dd2057959ce2badf89a7a
yjp8842 <yjp20183029@gmail.com>
2024-06-07 11:39:10 +0900

    feat: add animation to bottomup modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import './App.css';
-import Product from './components/common/Product/Product';
 
 function App() {
   return (
     <>
-      
     </>
   );
 }
--- src/components/common/Modal/BottomUpModal.tsx
+++ src/components/common/Modal/BottomUpModal.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import tw, { styled } from "twin.macro";
+import tw, { css, styled } from "twin.macro";
 
 import Close from '../../../assets/close.svg';
 
@@ -8,13 +8,26 @@ interface ModalProps {
   onClose: () => void;
 }
 
-
 const BackDrop = styled.div`
   ${tw`w-[100%] h-[100%] bg-black40 fixed left-0 top-0`}
 `;
 
+const slideUpAnimation = css`
+  @keyframes slideUp {
+    from {
+      transform: translateY(100%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+`;
+
 const ModalContainer = styled.div`
-  ${tw`w-[calc(100% - 40px)] px-5 py-4 gap-4 bg-gray-light fixed left-0 bottom-0 rounded-t-20 shadow-[2px_2px_12px_0_rgba(0,0,0,0.2)]`}
+  ${tw`flex flex-col w-[calc(100% - 40px)] h-fit px-5 py-4 gap-4 bg-gray-light fixed left-0 bottom-0 rounded-t-20 shadow-[2px_2px_12px_0_rgba(0,0,0,0.2)]`}
+  transition: height 0.3s ease-in-out;
+  animation: slideUp 0.5s ease-out forwards;
+  ${slideUpAnimation}
 `;
 
 const ItemContainer = styled.div`

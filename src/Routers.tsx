d04ff7e39468ce475d7516123a40c613dc3fa460
yjp8842 <yjp20183029@gmail.com>
2024-06-09 20:24:50 +0900

    feat: modify routers

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import EnergyMain from "./pages/Energy/EnergyMain";
+import StockPage from "./pages/StockPage";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/energy" element={<EnergyMain />} />
+      <Route path="/stock" element={<StockPage />} />
     </Routes>
   )
 }

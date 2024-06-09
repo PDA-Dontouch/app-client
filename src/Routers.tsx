d28ebe495b8ba7ad7dc7626dad49492e22d42675
yjp8842 <yjp20183029@gmail.com>
2024-06-09 20:48:30 +0900

    feat: add login route

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import EnergyMain from "./pages/Energy/EnergyMain";
 import StockPage from "./pages/StockPage";
+import Login from "./pages/Login";
 
 export default function Routers() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/energy" element={<EnergyMain />} />
       <Route path="/stock" element={<StockPage />} />
     </Routes>

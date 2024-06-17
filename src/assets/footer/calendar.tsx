5f7787f67c87ffc9f835f8d9cfdb6bdbd8d7359f
yjp8842 <yjp20183029@gmail.com>
2024-06-17 20:37:54 +0900

    design: modify footer color

---
+++ src/assets/footer/calendar.tsx
@@ -0,0 +1,46 @@
+interface CalendarIconProps {
+  isSelect: boolean;
+}
+
+const CalendarIcon = ({ isSelect }: CalendarIconProps) => {
+  return (
+    <svg
+      width="30"
+      height="31"
+      viewBox="0 0 30 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="3.75"
+        y="8"
+        width="22.5"
+        height="18.75"
+        rx="2"
+        stroke={isSelect ? '#1aa76e' : '#222222'}
+      />
+      <path
+        d="M3.75 14.25L26.25 14.25"
+        stroke={isSelect ? '#1aa76e' : '#222222'}
+        stroke-linecap="round"
+      />
+      <path
+        d="M11.25 20.5H18.75"
+        stroke={isSelect ? '#1aa76e' : '#222222'}
+        stroke-linecap="round"
+      />
+      <path
+        d="M10 4.25L10 9.25"
+        stroke={isSelect ? '#1aa76e' : '#222222'}
+        stroke-linecap="round"
+      />
+      <path
+        d="M20 4.25L20 9.25"
+        stroke={isSelect ? '#1aa76e' : '#222222'}
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default CalendarIcon;

dc2fc8b51d7f08343ac39c9e45787ada3295db05
eehanseul <gkstmf616@gmail.com>
2024-06-18 00:22:13 +0900

    feat: connect redirectPage

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -2,18 +2,24 @@ import { Route, Routes } from 'react-router-dom';
 import EnergyMain from './pages/Energy/EnergyMain';
 import EstatesMain from './pages/Estates/EstatesMain';
 import EstatesDetail from './pages/Estates/EstatesDetail';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Login/LoginPage';
+import KakaoRedirectPage from './pages/Login/KakaoRedirectPage';
 import InvestTypeTest from './pages/InvestTypeTest';
 import CalendarPage from './pages/CalendarPage';
 import StockMainPage from './pages/Stock/StockMainPage';
 import TransactionResult from './pages/TransactionResult';
 import MainPage from './pages/MainPage';
 import StockDetailPage from './pages/Stock/StockDetailPage';
+import NaverRedirectPage from './pages/Login/NaverRedirectPage';
+import GoogleRedirectPage from './pages/Login/GoogleRedirectPage';
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/login/oauth2/code/kakao" element={<KakaoRedirectPage />}></Route>
+      <Route path="/login/oauth2/code/naver" element={<NaverRedirectPage />}></Route>
+      <Route path="/login/oauth2/code/google" element={<GoogleRedirectPage />}></Route>
       <Route path="/typetest" element={<InvestTypeTest />} />
       <Route path="/energy" element={<EnergyMain />} />
       <Route path="/estates" element={<EstatesMain />} />

34f095e4cb7fda6bd99464cba0fc4eda2dd3f7e9
yjp8842 <yjp20183029@gmail.com>
2024-06-17 19:36:18 +0900

    feat: add select button

--- src/pages/Energy/EnergyMain.tsx
+++ src/pages/Energy/EnergyMain.tsx
@@ -11,6 +11,7 @@ import Footer from '../../components/common/Footer';
 import Navbar from '../../components/common/Navbar';
 import SortButton from '../../components/common/SortButton';
 import Product from '../../components/common/Product/Product';
+import { EnergyList } from '../../types/energy_product';
 
 const Container = styled.div`
   ${tw`w-[calc(100% - 56px)] mt-14 mb-16 px-7 py-8 flex flex-col gap-5`}
@@ -20,20 +21,25 @@ const BtnContainer = styled.div`
   ${tw`flex w-full h-fit justify-end gap-3`}
 `;
 
-const MainText = styled.span`
+const MainText = styled.div`
   ${tw`w-fit text-xl`}
   ${css`
     box-shadow: inset 0 -10px 0 rgba(82, 147, 208, 0.5);
     line-height: 26px;
-  `}
+  `} // ${tw`w-full bg-blue50 mt-14 px-4 pt-[120px] pb-8 text-white font-semibold text-xl`}
 `;
 
-const SubText = styled.span`
+const SubText = styled.span<{ isSelect: boolean }>`
   ${tw`text-lg`}
+  ${({ isSelect }) => (isSelect ? tw`text-black` : tw`text-gray70`)}
 `;
 
 const ItemContainer = styled.div`
   ${tw`flex flex-col gap-8`}
+`;
+
+const SelectContainer = styled.div`
+  ${tw`flex gap-3`}
 `;
 
 const EnergyMain = () => {
@@ -41,21 +47,41 @@ const EnergyMain = () => {
   const navigate = useNavigate();
   const energyDatas = useSelector((state: RootState) => state.energy.datas);
   const [sortByProfit, setSortByProfit] = useState<boolean>(false);
+  const [isSelect, setIsSelect] = useState(0);
 
   useEffect(() => {
     dispatch(getEnergyDatas());
   }, []);
 
-  const sortDataByProfit = () => {
-    setSortByProfit(!sortByProfit);
-  };
-
-  const sortedData = sortByProfit
-    ? [...energyDatas].sort((a, b) => b.earningRate - a.earningRate)
-    : energyDatas;
-
   const { EstatesLikeArr, setLikeEstates, EnergyLikeArr, setLikeEnergy } =
     useLike();
+
+  const filterAndSortData = (data: EnergyList[], completed: boolean) => {
+    const filteredData = data.filter((energy) =>
+      completed
+        ? energy.sumOfInvestmentAndReservation === energy.fundingAmount
+        : energy.sumOfInvestmentAndReservation !== energy.fundingAmount,
+    );
+    return sortByProfit
+      ? filteredData.sort((a, b) => b.earningRate - a.earningRate)
+      : filteredData;
+  };
+
+  const renderProducts = (data: EnergyList[]) => {
+    return data.map((item) => (
+      <div key={item.energyId}>
+        <Product
+          isEstates={false}
+          data={item}
+          isLike={EnergyLikeArr.includes(item.energyId)}
+          setIsLike={() => setLikeEnergy(item.energyId)}
+        />
+      </div>
+    ));
+  };
+
+  const ongoingInvestments = filterAndSortData(energyDatas, false);
+  const completedInvestments = filterAndSortData(energyDatas, true);
 
   return (
     <>
@@ -73,21 +99,21 @@ const EnergyMain = () => {
             isEstates={false}
             isSelect={sortByProfit}
             title="수익률순"
-            onClick={sortDataByProfit}
+            onClick={() => setSortByProfit(true)}
           />
         </BtnContainer>
         <ItemContainer>
-          <SubText>모집 중</SubText>
-          {sortedData.map((item, idx) => (
-            <div key={item.energyId}>
-              <Product
-                isEstates={false}
-                data={item}
-                isLike={EnergyLikeArr.includes(item.energyId) ? true : false}
-                setIsLike={() => setLikeEnergy(item.energyId)}
-              />
-            </div>
-          ))}
+          <SelectContainer>
+            <SubText isSelect={isSelect === 0} onClick={() => setIsSelect(0)}>
+              모집 중
+            </SubText>
+            <SubText isSelect={isSelect === 1} onClick={() => setIsSelect(1)}>
+              모집 완료
+            </SubText>
+          </SelectContainer>
+          {isSelect === 0
+            ? renderProducts(ongoingInvestments)
+            : renderProducts(completedInvestments)}
         </ItemContainer>
       </Container>
       <Footer />
